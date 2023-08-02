af012ded1fb9fca259b94b1754f4ca17c2149d88
krishna-123-star <68377201+krishna-123-star@users.noreply.github.com>
2023-08-02 15:14:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+//Arrow function to calculate area of triangle
+
+var areaOfTriangle = (base, height) => (base * height)/2
+
+const base = 10
+const height = 6
+
+var areaOfTriangle = areaOfTriangle(base,height)
+
+console.log("Area of Traiangle: " + areaOfTriangle)
